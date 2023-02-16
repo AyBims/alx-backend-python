6a30edc21ec9abfa9eb487e69187a631758e25f5
ayomide <adeshinaayomide12@yahoo.com>
2023-02-16 17:25:00 +0100

    Update: linting errors

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -2,6 +2,7 @@
 """0-add.py"""
 """Module that contains a single function that adds two integers."""
 
-def add(a: float, b: float) -> float: 
+
+def add(a: float, b: float) -> float:
     """addition of two floats"""
     return a + b
