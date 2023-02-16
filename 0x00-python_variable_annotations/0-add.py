07e1b0fa6302fbbabb7c3423af5ddd4eab8d1013
ayomide <adeshinaayomide12@yahoo.com>
2023-02-16 17:20:44 +0100

    Update: task 0 correction documentation

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
+"""0-add.py"""
 """Module that contains a single function that adds two integers."""
 
 def add(a: float, b: float) -> float: 
+    """addition of two floats"""
     return a + b
